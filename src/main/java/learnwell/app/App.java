42a5c34b90521bedb1cb8a176f53c203729d8020
bhagyashri-cj <70849125+bhagyashri-cj@users.noreply.github.com>
2022-09-23 17:15:25 +0530

    Update App.java

--- src/main/java/learnwell/app/App.java
+++ src/main/java/learnwell/app/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello Docker jenkins this is new freestyle project!" );
+        System.out.println( "Hello World!" );
     }
 }
